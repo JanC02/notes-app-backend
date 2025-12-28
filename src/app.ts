edb8e1c160fb3aa72c1b4026f59bdcff7a629486
Jan Chrobak <jan.chrobak02@outlook.com>
2025-12-28 21:37:28 +0100

    add error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import { appConfig } from "./config/config.js";
 import { cors } from "./middlewares/cors.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
 import express from "express";
 
 const app = express();
 
-app.use(express.json());
 app.use(cors);
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json({message: 'Hello World!'});
 });
+
+app.use(errorHandler);
 
 app.listen(appConfig.port, () => {
     console.log(`Listening on port ${appConfig.port}`);
