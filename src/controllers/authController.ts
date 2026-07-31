31afcb6c5e0f19ace5fe1fd40ca54f4741d14e5b
JanC02 <jan.chrobak02@outlook.com>
2026-07-31 21:43:06 +0200

    clear cookies

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,8 +1,9 @@
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 import { ApiError } from "../types/errors/ApiError.js";
 import { registerUserSchema, loginUserSchema } from "../types/user.js";
 import { type RefreshToken } from "../types/auth.js";
 import * as authService from "../services/authService.js";
+import { InvalidTokenError } from "../types/errors/InvalidTokenError.js";
 
 export async function register(req: Request, res: Response) {
     const parseResult = registerUserSchema.safeParse(req.body);
@@ -37,27 +38,45 @@ export async function login(req: Request, res: Response) {
 export async function logout(req: Request, res: Response) {
     const refreshToken: RefreshToken = req.cookies.refreshToken;
 
-    if (!refreshToken) {
-        throw new ApiError(400, 'Token is required');
+    if (refreshToken) {
+        await authService.logout(refreshToken);
     }
 
-    await authService.logout(refreshToken);
-    res.sendStatus(204);
+    res.status(204).clearCookie('refreshToken', {
+        httpOnly: true,
+        path: '/api/auth',
+        sameSite: 'strict',
+        secure: process.env.NODE_ENV === 'production' ? true : false
+    }).end();
 }
 
-export async function refresh(req: Request, res: Response) {
+export async function refresh(req: Request, res: Response, next: NextFunction) {
     const refreshToken: RefreshToken = req.cookies.refreshToken;
 
     if (!refreshToken) {
-        throw new ApiError(400, 'Token is required');
+        throw new ApiError(401, 'Unauthorized');
     }
 
-    const result = await authService.refresh(refreshToken);
-    res.cookie('refreshToken', result.refreshToken, {
-            httpOnly: true,
-            expires: result.exp,
-            path: '/api/auth',
-            sameSite: 'strict',
-            secure: process.env.NODE_ENV === 'production' ? true : false
-        }).json({ accessToken: result.accessToken });
+    try {
+        const result = await authService.refresh(refreshToken);
+        res
+            .cookie('refreshToken', result.refreshToken, {
+                httpOnly: true,
+                expires: result.exp,
+                path: '/api/auth',
+                sameSite: 'strict',
+                secure: process.env.NODE_ENV === 'production' ? true : false
+            })
+            .json({ accessToken: result.accessToken });
+    } catch (error) {
+        if (error instanceof InvalidTokenError) {
+            res.clearCookie('refreshToken', {
+                httpOnly: true,
+                path: '/api/auth',
+                sameSite: 'strict',
+                secure: process.env.NODE_ENV === 'production' ? true : false
+            });
+        }
+        return next(error);
+    }
 }
